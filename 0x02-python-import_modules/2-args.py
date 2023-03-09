adb7e8b83b5ce4d6082ac8b28fbe989114adde8d
marthaakinlolu <marthaakinlolu@gmail.com>
2023-03-09 07:38:26 -0800

    2-args.py

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    import sys
+    args = sys.argv[1:]
+    num_args = len(args)
+    
+    if num_args == 0:
+        print("{} arguments.".format(num_args))
+    elif num_args == 1:
+        print("{} argument:".format(num_args))
+    else:
+        print("{} arguments:".format(num_args))
+
+    if num_args >= 1:
+        i = 1
+        for arg in args:
+            print("{}: {}".format(i, arg))
+            i += 1
