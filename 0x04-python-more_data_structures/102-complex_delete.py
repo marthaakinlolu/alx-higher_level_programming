733546ffc8327724339a0036e0184e3263af5f66
marthaakinlolu <marthaakinlolu@gmail.com>
2023-03-15 14:46:40 -0700

    update

--- 0x04-python-more_data_structures/102-complex_delete.py
+++ 0x04-python-more_data_structures/102-complex_delete.py
@@ -6,3 +6,4 @@ def complex_delete(a_dictionary, value):
             keys_to_delete.append(key)
     for key in keys_to_delete:
         del a_dictionary[key]
+    return a_dictionary
--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
 def roman_to_int(roman_string):
-    if not roman_string or not isinstance(roman_string, str):
-        # Return 0 if the input is None or not a string
+    if type(roman_string) is not str or roman_string is None:
         return 0
 
     roman_dict = {
@@ -14,17 +13,15 @@ def roman_to_int(roman_string):
         'M': 1000
     }
 
-    # Initialize the result to 0
+    dec = [roman_dict[x] for x in roman_string]
     result = 0
 
-    for i in range(len(roman_string)):
+    for i in range(len(dec)):
         # Get the integer value of the current Roman numeral
-        current_value = roman_dict.get(roman_string[i])
+        result += dec[i]
 
         # If the next numeral is of greater value, subtract the current value
-        if i < len(roman_string) - 1 and roman_dict.get(roman_string[i+1]) > current_value:
-            result -= current_value
-        else:
-            result += current_value
+        if dec[i - 1] < dec[i] and i != 0:
+            result -= (dec[i - 1] + dec[i - 1])
 
     return result
