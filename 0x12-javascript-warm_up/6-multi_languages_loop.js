1252ba7ed17daa6ff063c0fbeadc6bcbbb163545
marthaakinlolu <marthaakinlolu@gmail.com>
2023-06-15 05:55:18 -0700

    task6 update

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,4 +1,5 @@
 #!/usr/bin/node
-for (const line of ['C is fun', 'Python is cool', 'JavaScript is amazing']) {
-  console.log(line);
+lang = ['C is fun', 'Python is cool', 'JavaScript is amazing']
+for (let i = 0; i < lang.length; i++) {
+  console.log(lang[i]);
 }
