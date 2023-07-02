9812e2aecf4fa0bbc1edc44fec6851b90930ff07
marthaakinlolu <marthaakinlolu@gmail.com>
2023-07-02 15:20:16 -0700

    update

--- 0x10-python-network_0/102-catch_me.sh
+++ 0x10-python-network_0/102-catch_me.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-# makes a request to 0.0.0.0:5000/catch_me for a response "You got me!"
-curl -s "You got me!" 0.0.0.0:5000/catch_me
+# Makes a request to 0.0.0.0:5000/catch_me
+curl -sL -X PUT -H "Origin: HolbertonSchool" -d "user_id=98" 0.0.0.0:5000/catch_me
