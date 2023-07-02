ccf45fc216185dfaeca31e73ef872f48f7f6ad7f
marthaakinlolu <marthaakinlolu@gmail.com>
2023-07-02 15:27:16 -0700

    update

--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -1,27 +1,28 @@
 #!/usr/bin/python3
 """
-Use requests package to make a post request to given URL with argument
-set in variable `q`, defaulting to empty string. If response body is properly
-JSON formatted and not empty, display `id` and `name` as given format.
-Otherwise display error message.
+script that takes in a letter and sends a POST request to
+http://0.0.0.0:5000/search_user with the letter as a parameter.
 """
-import sys
-import requests
 
+import requests
+import sys
 if __name__ == "__main__":
     if len(sys.argv) > 1:
-        arg = sys.argv[1]
+        letter = sys.argv[1]
     else:
-        arg = ""
-    payload = {'q': arg}
+        letter = ""
+
     url = "http://0.0.0.0:5000/search_user"
-    r = requests.post(url, data=payload)
+    payload = {"q": letter}
+
+    response = requests.post(url, data=payload)
+
     try:
-        r.raise_for_status()
-        json = r.json()
-        if len(json) == 0:
-            print("No result")
+        json_data = response.json()
+
+        if json_data:
+            print("[{}] {}".format(json_data['id'], json_data['name']))
         else:
-            print("[{:d}] {}".format(json['id'], json['name']))
-    except Exception:
+            print("No result")
+    except ValueError:
         print("Not a valid JSON")
