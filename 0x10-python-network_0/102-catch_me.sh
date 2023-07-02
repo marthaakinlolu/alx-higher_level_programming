a7cd53119d6efc34437fb980fe5fb9f4c83de695
marthaakinlolu <marthaakinlolu@gmail.com>
2023-07-02 15:13:00 -0700

    update

--- 0x10-python-network_0/102-catch_me.sh
+++ 0x10-python-network_0/102-catch_me.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-# makes a request to 0.0.0.0:5000/catch_me that get the message You got me!
-curl -s -o /dev/null -w "You got me!" 0.0.0.0:5000/catch_me
+# makes a request to 0.0.0.0:5000/catch_me for a response "You got me!"
+curl -sX PUT -L -d  "You got me!" 0.0.0.0:5000/catch_me
